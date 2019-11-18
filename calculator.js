0b775c7eefe6039f91c5df8011b99ddefee959e2
Vivek Panchal <vicky.vivek14@gmail.com>
2019-11-18 12:24:19 -0500

    Initial Load

---
+++ calculator.js
@@ -0,0 +1,28 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(request,response){
+	response.sendFile(__dirname + "/index.html");
+	});
+app.post("/",function(request,response){
+	var num1 = Number(request.body.num1);
+	var num2 = Number(request.body.num2);
+	var result = num1 + num2;
+	response.send("The calculation is :" +result);
+	});
+
+app.get("/bmicalculator",function(request,response){
+	response.sendFile(__dirname + "/bmiCalculator.html");
+	});	
+app.post("/bmicalculator",function(request,response){
+	var weight = parseFloat(request.body.weight);
+	var height = parseFloat(request.body.height);
+	var bmi = weight/(height * height);
+	response.send("Your BMI is :" +bmi);
+	});	
+app.listen(4040,function(){
+	console.log("Server started on port 4040");
+});
